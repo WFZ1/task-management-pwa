d9ebac877d2136767a4f3d38b399d6b123167ff6
WFZ1 <pavelskumenko@gmail.com>
2024-06-17 20:49:11 +0300

    feat: add styles for mobile view

--- src/components/ui/data-table/data-table-pagination.tsx
+++ src/components/ui/data-table/data-table-pagination.tsx
@@ -9,12 +9,12 @@ interface DataTablePaginationProps<TData> {
 
 export function DataTablePagination<TData>({ table }: DataTablePaginationProps<TData>) {
     return (
-        <div className="flex items-center justify-between px-2">
+        <div className="flex flex-wrap flex-col min-[400px]:flex-row items-center justify-between px-2 space-y-2 min-[400px]:space-y-0">
             <div className="flex-1 text-sm text-muted-foreground">
                 {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s)
                 selected.
             </div>
-            <div className="flex items-center space-x-6 lg:space-x-8">
+            <div className="flex flex-col min-[400px]:flex-row items-center space-x-6 lg:space-x-8 space-y-2 min-[400px]:space-y-0">
                 <div className="flex items-center space-x-2">
                     <p className="text-sm font-medium">Rows per page</p>
                     <Select
--- src/components/ui/data-table/data-table-toolbar.tsx
+++ src/components/ui/data-table/data-table-toolbar.tsx
@@ -16,34 +16,32 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
     const isFiltered = table.getState().columnFilters.length > 0;
 
     return (
-        <div className="flex items-center justify-between">
-            <div className="flex flex-1 items-center space-x-2">
-                <Input
-                    placeholder="Filter by title..."
-                    value={(table.getColumn('title')?.getFilterValue() as string) ?? ''}
-                    onChange={(event) => table.getColumn('title')?.setFilterValue(event.target.value)}
-                    className="h-8 w-[150px] lg:w-[250px]"
-                />
-                <DatePicker
-                    placeholder="Filter by date..."
-                    value={
-                        table.getColumn('deadline')?.getFilterValue()
-                            ? new Date(table.getColumn('deadline')?.getFilterValue() as string)
-                            : undefined
-                    }
-                    onChange={(value) => {
-                        table.getColumn('deadline')?.setFilterValue(value && format(value, DATE_FORMAT));
-                    }}
-                />
-                <DataTableFacetedFilter column={table.getColumn('priority')} title="Priority" options={PRIORITIES} />
-                <DataTableFacetedFilter column={table.getColumn('isCompleted')} title="Completed" options={COMPLETED} />
-                {isFiltered && (
-                    <Button variant="ghost" onClick={() => table.resetColumnFilters()} className="h-8 px-2 lg:px-3">
-                        Reset
-                        <Cross2Icon className="ml-2 h-4 w-4" />
-                    </Button>
-                )}
-            </div>
+        <div className="flex flex-wrap flex-1 items-center space-x-2 space-y-2 lg:space-y-0">
+            <Input
+                placeholder="Filter by title..."
+                value={(table.getColumn('title')?.getFilterValue() as string) ?? ''}
+                onChange={(event) => table.getColumn('title')?.setFilterValue(event.target.value)}
+                className="h-8 w-[250px]"
+            />
+            <DatePicker
+                placeholder="Filter by date..."
+                value={
+                    table.getColumn('deadline')?.getFilterValue()
+                        ? new Date(table.getColumn('deadline')?.getFilterValue() as string)
+                        : undefined
+                }
+                onChange={(value) => {
+                    table.getColumn('deadline')?.setFilterValue(value && format(value, DATE_FORMAT));
+                }}
+            />
+            <DataTableFacetedFilter column={table.getColumn('priority')} title="Priority" options={PRIORITIES} />
+            <DataTableFacetedFilter column={table.getColumn('isCompleted')} title="Completed" options={COMPLETED} />
+            {isFiltered && (
+                <Button variant="ghost" onClick={() => table.resetColumnFilters()} className="h-8 px-2 lg:px-3">
+                    Reset
+                    <Cross2Icon className="ml-2 h-4 w-4" />
+                </Button>
+            )}
         </div>
     );
 }
