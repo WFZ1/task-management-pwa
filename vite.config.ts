10b79c6e902b34b01f24a37af58b3bfd132b2beb
WFZ1 <pavelskumenko@gmail.com>
2024-06-20 10:17:52 +0300

    feat: configure pwa with vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: manifest as Partial<ManifestOptions>,
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
